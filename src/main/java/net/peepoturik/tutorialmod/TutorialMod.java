dcc8ee67e19dfae7981263ba54a0608655dd41e0
peepoTurik <amirhaiman48@gmail.com>
2025-01-19 10:36:07 +0800

    Added new blocks

--- src/main/java/net/peepoturik/tutorialmod/TutorialMod.java
+++ src/main/java/net/peepoturik/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.peepoturik.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.peepoturik.tutorialmod.blocks.ModBlocks;
 import net.peepoturik.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,5 +15,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/peepoturik/tutorialmod/blocks/ModBlocks.java
@@ -0,0 +1,45 @@
+package net.peepoturik.tutorialmod.blocks;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.peepoturik.tutorialmod.TutorialMod;
+
+public class ModBlocks {
+
+    public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                .requiresTool()
+                .sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+    public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
+            new Block(AbstractBlock.Settings.create().strength(3f)
+                    .requiresTool()));
+
+    //RegisterBlock Helper Method
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(TutorialMod.MOD_ID, name), block);
+    }
+
+    //RegisterBlockItem Helper Method
+    private static void registerBlockItem(String name, Block block){
+        Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.info("Register Mod Blocks for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
+                fabricItemGroupEntries.add(ModBlocks.PINK_GARNET_BLOCK);
+                fabricItemGroupEntries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+        });
+    }
+}
